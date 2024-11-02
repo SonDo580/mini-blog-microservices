16e87aef110e399c332c4d007c1d7504c9491738
Son Do <dominhson580@gmail.com>
2024-11-02 09:24:35 +0700

    comments service

---
+++ comments/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+
+const app = express();
+app.use(express.json());
+
+const commentsInPost = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsInPost[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  // check posts exists
+
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  const comments = commentsInPost[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsInPost[req.params.id] = comments;
+
+  res.status(201).json(comments);
+});
+
+const PORT = 4001;
+app.listen(PORT, () => {
+  `Server listening on port ${PORT}`;
+});
